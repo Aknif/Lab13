d5ec9e8c9f57e86b9f32d9e4d461b7af343cc938
Aknif <apichai5244@gmail.com>
2022-10-11 17:51:07 +0700

    FizzBuzz

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	Fizzbuzz fizzbuzz = new Fizzbuzz();
+	
+	@org.junit.jupiter.api.Test
+	void TC01_input=3_Ex=Fizz() { 
+		assertEquals("Fizz", input);
+	}
+
+}
