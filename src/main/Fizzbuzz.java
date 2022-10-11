d28abd0ab82da7c7c583583e27b9210ef4a79f06
SiriLix <benswordsiri@hotmail.com>
2022-10-11 21:36:52 +0700

    Main + testcase05 Num = 101 - Pass

--- src/main/Fizzbuzz.java
+++ src/main/Fizzbuzz.java
@@ -8,15 +8,18 @@ public class Fizzbuzz {
 	public String anotherFizzBuzz(int i) {
 		String s = "";
 			
-			if (i%5 ==0 && i%3!=0) {
-				s = Integer.toString(i);
+			if(i>100){
+                s="Input number more than 100";
+            }
+            else if (i%5 ==0 && i%3!=0) {
+				
 				s = "Buzz";
 			}else if (i%3 == 0&& i%5!=0) {
-				s = Integer.toString(i);
+				
 				s = "Fizz";	
 			}
 			else if(i%3 == 0 && i%5 ==0) {
-				s = Integer.toString(i);
+				
 				s = "FizzBuzz";
 			}
 			else {
--- src/test/Test.java
+++ src/test/Test.java
@@ -47,7 +47,7 @@ class test {
     }
 	
 	@Test
-    @DisplayName("TC05 - input = 101, expected result = Input numer more than 100")
+    @DisplayName("TC05 - input = 101, expected result = Input number more than 100")
     void TC05() {
         String testfb = fb.anotherFizzBuzz(101);
         assertEquals("Input numer more than 100", testfb);   
